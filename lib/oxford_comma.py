1c649ec4469353f4c48814127a1e9d5cf08659c1
Gerald-GG <gicharugerald@gmail.com>
2023-11-28 12:12:19 +0300

    completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(words):
+    if len(words) == 1:
+        return words[0]
+    elif len(words) == 2:
+        return f"{words[0]} and {words[1]}"
+    else:
+        formatted_words = ', '.join(words[:-1])
+        return f"{formatted_words}, and {words[-1]}"
+result = oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+print(result)
